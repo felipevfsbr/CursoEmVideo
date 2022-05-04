8459acca882e56710507a1257426a03a447eab56
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-04 11:04:56 -0300

    Create Desafio07.py

---
+++ Desafio07.py
@@ -0,0 +1,7 @@
+# Author: Néris
+
+aluno = str(input('Digite o nome do Aluno:'))
+n1 = float(input('Digite a Primeira nota do aluno:'))
+n2 = float(input('Digite a Segunda nota do aluno:'))
+
+print('A média do aluno {} é:{}'.format(aluno, ((n1+n2)/2)))
