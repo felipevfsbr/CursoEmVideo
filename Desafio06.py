af3128453afe2d35c02f487b991ff5a6843ff955
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-04 10:46:02 -0300

    Create Desafio06.py

---
+++ Desafio06.py
@@ -0,0 +1,7 @@
+# Author: Néris
+
+n=int(input('Digite um Numero:'))
+
+print("""O Dobro de {} é {}.
+O triplo de {} é {}
+A Raiz Quadrada de {} é {}""".format(n, (n*2), n, (n*3), n, int(n**(1/2))))
