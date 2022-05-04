10fd9b934c90a26177d65914737d3fbf079ad171
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-04 10:39:03 -0300

    Create Desafio05.py

---
+++ Desafio05.py
@@ -0,0 +1,7 @@
+# Author:Néris
+
+n = int(input('Insira um numero:'))
+n1 = (n - 1)
+n2 = (n + 1)
+
+print('O numero que voce digitou é {}, o antecessor dele é {} e o sucessor dele é {}'.format(n, n1, n2))
