ac3f819c44ae3e7aa9bf5d0af5107ef460c1e0fc
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-05 13:33:44 -0300

    Create Desafio08.py

---
+++ Desafio08.py
@@ -0,0 +1,11 @@
+# Author:Néris
+
+n = float(input('Digite um Valor em Metro:'))
+
+print('O Valor em milimetro é {}mm'.format(n*1000))
+print('O Valor em centimetro é {}cm'.format(n*100))
+print('O Valor em decimetro é {}dm'.format(n*10))
+print('O Valor em metro é {}m'.format(n))
+print('O Valor em decametro é {}dc'.format(n/10))
+print('O Valor em hectometro é {}hm'.format(n/100))
+print('O Valor em quilometro é {}km'.format(n/1000))
