a0693dd85e543a1ce4c3b982e79c26852fa009af
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-06 09:29:23 -0300

    Create Desafio14.py

---
+++ Desafio14.py
@@ -0,0 +1,6 @@
+# author : Néris
+
+celsius = float(input('Qual a temperatura em Graus Celsius? '))
+fh = float(celsius * 1.8) + 32
+
+print('A temperatura de {} Graus Celsius, convertida para Fahrenheit tem como resultado {}F'.format(celsius, fh))
