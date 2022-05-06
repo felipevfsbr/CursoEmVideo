27d0d9eed64bf8a5973c172bea9bcc05921bc343
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-06 13:45:00 -0300

    Create Desafio17 sem modulo.py

---
+++ Desafio17 sem modulo.py
@@ -0,0 +1,6 @@
+#author: Néris
+co = float(input('Comprimento do caceto oposto: '))
+ca = float(input('Comprimento do caceto adjacente: '))
+hp = float(co**2)+(ca**2)
+
+print('A hipotenusa vai medir {:.2f}'.format(hp**0.5))
